cfb2b6a30d04d7ce187b27c537016a55d89c405d
Arthur Cobbaert <acobbaer@a2-r3-p6.s19.be>
2025-11-04 19:34:03 +0100

    b

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: acobbaer <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 18:07:19 by acobbaer          #+#    #+#             */
-/*   Updated: 2025/11/04 18:48:25 by acobbaer         ###   ########.fr       */
+/*   Updated: 2025/11/04 19:33:46 by acobbaer         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_calloc(size_t count, size_t size)
 	{
 		return (NULL);
 	}
-	total_bytes = count size;
+	total_bytes = count * size;
 	ptr = malloc(total_bytes);
 	if (ptr == NULL)
 	{
